b9ba7d466d6435cdfa56a48e2a1bf42871437ac1
elebillio <110589389+elebillio@users.noreply.github.com>
2023-04-02 14:48:55 +0500

    form1

--- Csmd Attendence/Form1.Designer.vb
+++ Csmd Attendence/Form1.Designer.vb
@@ -24,6 +24,7 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.mK = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.p1 = New System.Windows.Forms.ProgressBar()
         Me.SuspendLayout()
         '
         'mK
@@ -43,11 +44,19 @@ Partial Class Form1
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'p1
+        '
+        Me.p1.Location = New System.Drawing.Point(84, 74)
+        Me.p1.Name = "p1"
+        Me.p1.Size = New System.Drawing.Size(442, 23)
+        Me.p1.TabIndex = 2
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(700, 382)
+        Me.Controls.Add(Me.p1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.mK)
         Me.Name = "Form1"
@@ -59,4 +68,5 @@ Partial Class Form1
 
     Friend WithEvents mK As TextBox
     Friend WithEvents Button1 As Button
+    Friend WithEvents p1 As ProgressBar
 End Class
--- Csmd Attendence/Form1.vb
+++ Csmd Attendence/Form1.vb
@@ -10,33 +10,39 @@ Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Using db As New CsmdBioAttendenceEntities
             Dim idX As Integer = CInt(mK.Text)
-            Dim dt = (From a In db.Employees Where a.User_ID = 2 And a.Emp_ID = idX Select a).FirstOrDefault
-            If dt IsNot Nothing Then
-                Dim df = (From a In dt.Emp_Bio_Device_Users Select a).ToList
-                If df.Count > 0 Then
-                    For Each dfs In df
-                        Dim dg = (From a In dfs.Emp_Attendence_Device Select a).ToList
-                        If dg.Count > 0 Then
-                            For Each dgs In dg
-                                db.Emp_Attendence_Device.Remove(dgs)
-                            Next
-                        End If
-                        db.Emp_Bio_Device_Users.Remove(dfs)
-                    Next
-                End If
+            Dim dt = (From a In db.Employees Where a.User_ID = 2 Select a).ToList
+            If dt.Count > 0 Then
+                p1.Maximum = dt.Count - 1
+                p1.Value = 0
+                For Each dta In dt
+                    Dim df = (From a In dta.Emp_Bio_Device_Users Select a).ToList
+                    If df.Count > 0 Then
+                        For Each dfs In df
+                            Dim dg = (From a In dfs.Emp_Attendence_Device Select a).ToList
+                            If dg.Count > 0 Then
+                                For Each dgs In dg
+                                    db.Emp_Attendence_Device.Remove(dgs)
+                                Next
+                            End If
+                            db.Emp_Bio_Device_Users.Remove(dfs)
+                        Next
+                    End If
 
 
-                Dim dh = (From a In dt.Emp_Att_Payment Select a).ToList
-                If dh.Count > 0 Then
-                    For Each dhs In dh
-                        db.Emp_Att_Payment.Remove(dhs)
-                    Next
-                End If
+                    Dim dh = (From a In dta.Emp_Att_Payment Select a).ToList
+                    If dh.Count > 0 Then
+                        For Each dhs In dh
+                            db.Emp_Att_Payment.Remove(dhs)
+                        Next
+                    End If
+                    Me.Text = dta.Emp_ID & " " & dta.Emp_Name
+                    db.Employees.Remove(dta)
+                    p1.Value = p1.Value + 1
+                    Application.DoEvents()
+                Next
 
 
 
-
-                db.Employees.Remove(dt)
                 db.SaveChanges()
                 MsgBox("Delete Record Successfull")
             Else
